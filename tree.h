0aa76ecf8a00e18f93e0d00654e4aa7297ca033f
Hermano Castro <hermanocastro18@gmail.com>
2023-09-30 13:30:12 -0300

    create a header with tree structs and functions

---
+++ tree.h
@@ -0,0 +1,20 @@
+typedef struct student {
+  int registration;
+  char name[25];
+  char class [12];
+}
+Student;
+
+typedef struct tree_node {
+  Student student;
+  struct tree_node * right;
+  struct tree_node * left;
+}
+Node;
+
+int is_empty(Node * node);
+int test_is_empty(void);
+
+Node * initialize_tree(void);
+int test_initialize_tree(void);
+
