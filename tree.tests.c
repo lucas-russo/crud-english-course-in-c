778ffb386983d47460460473f793b3c3eda3db73
Hermano Castro <hermanocastro18@gmail.com>
2023-09-30 13:31:14 -0300

    create tests for functions is_empty and initialize_tree

---
+++ tree.tests.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tree.h"
+
+int test_is_empty() {
+  Node * empty_node = NULL;
+  Node node;
+  node.student.registration = 42702;
+  node.left = NULL;
+  node.right = NULL;
+
+  if (is_empty(empty_node)) printf("Test for empty node: Passed\n");
+  else printf("Test for empty node: Failed\n");
+
+  if (!is_empty(&node)) printf("Test for not empty node: Passed\n");
+  else printf("Test for not empty node: Failed\n");
+
+  return 0;
+}
+
+int test_initialize_tree() {
+  Node * tree = initialize_tree();
+
+  if (is_empty(tree)) printf("Test initialize tree: Passed\n");
+  else printf("Test initialize tree: Failed\n");
+
+  free(tree);
+
+  return 0;
+}
