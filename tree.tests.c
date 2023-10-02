c882424be56715a0fc2592916918808903e2363c
Hermano Castro <hermanocastro18@gmail.com>
2023-10-02 16:07:04 -0300

    aply mocks on tests

--- tree.tests.c
+++ tree.tests.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 #include "constants.h"
-#include "tree.h"
+#include "mocks.h"
 
 int test_number = 0;
 
@@ -38,7 +38,6 @@ int test_constants(void) {
   for (i = 0; i < 4; i++) {
     printf("%d: %s\n", i, language_types[i]);
 
-    // Comparar com a string esperada
     if (strcmp(language_types[i], "English") == 0)
       printf("Language is correct: English\n");
     else if (strcmp(language_types[i], "Spanish") == 0)
@@ -91,31 +90,25 @@ int test_initialize_tree() {
 }
 
 int test_free_tree() {
+  initialize_students_mocks();
+
   Node* node1 = (Node*)malloc(sizeof(Node));
-  node1->student.registration = 12702;
-  strcpy(node1->student.name, "Alice");
-  strcpy(node1->student.class_level, class_level_types[2]);
-  strcpy(node1->student.language, language_types[0]);
+  node1->student = student1;
   node1->left = NULL;
   node1->right = NULL;
 
   Node* node2 = (Node*)malloc(sizeof(Node));
-  node2->student.registration = 24270;
-  strcpy(node2->student.name, "Bob");
-  strcpy(node2->student.class_level, class_level_types[0]);
-  strcpy(node2->student.language, language_types[1]);
+  node2->student = student2;
   node2->left = NULL;
   node2->right = NULL;
 
   Node* root = (Node*)malloc(sizeof(Node));
-  root->student.registration = 34272;
-  strcpy(root->student.name, "Charlie");
-  strcpy(root->student.class_level, class_level_types[1]);
-  strcpy(root->student.language, language_types[2]);
-  
+  root->student = student3;
+
   root->left = node1;
   root->right = node2;
 
+  free_all_students_mocks();
   free_tree(root);
 
   printf("Test free_tree: Passed\n");
@@ -140,13 +133,9 @@ int test_generate_registration() {
 }
 
 int test_create_node() {
-  Student student;
-  student.registration = generate_registration();
-  strncpy(student.name, "Alice", sizeof(student.name));
-  strncpy(student.class_level, class_level_types[2],
-          sizeof(student.class_level));
-  strncpy(student.language, language_types[0], sizeof(student.language));
-
+  initialize_students_mocks();
+  
+  Student student = student1;
   Node* left = create_node(student, NULL, NULL);
   Node* right = create_node(student, NULL, NULL);
   Node* root = create_node(student, left, right);
@@ -155,7 +144,8 @@ int test_create_node() {
     printf("Test create_node: Failed\n");
     return 1;
   }
-
+  
+  free_all_students_mocks();
   free_tree(root);
 
   printf("Test create_node: Passed\n");
@@ -187,77 +177,9 @@ int is_tree_alphabetical(Node* node) {
 }
 
 int test_include_student(void) {
+  initialize_students_mocks();
+
   Node* root = initialize_tree();
-
-  Student student1;
-  student1.registration = generate_registration();
-  strncpy(student1.name, "Zoe", sizeof(student1.name));
-  strncpy(student1.class_level, class_level_types[0],
-          sizeof(student1.class_level));
-  strncpy(student1.language, language_types[2], sizeof(student1.language));
-
-  Student student2;
-  student2.registration = generate_registration();
-  strncpy(student2.name, "Alice", sizeof(student2.name));
-  strncpy(student2.class_level, class_level_types[2],
-          sizeof(student2.class_level));
-  strncpy(student2.language, language_types[0], sizeof(student2.language));
-
-  Student student3;
-  student3.registration = generate_registration();
-  strncpy(student3.name, "Charlie", sizeof(student3.name));
-  strncpy(student3.class_level, class_level_types[1],
-          sizeof(student3.class_level));
-  strncpy(student3.language, language_types[2], sizeof(student3.language));
-
-  Student student4;
-  student4.registration = generate_registration();
-  strncpy(student4.name, "Yasmine", sizeof(student4.name));
-  strncpy(student4.class_level, class_level_types[1],
-          sizeof(student4.class_level));
-  strncpy(student4.language, language_types[0], sizeof(student4.language));
-
-  Student student5;
-  student5.registration = generate_registration();
-  strncpy(student5.name, "Anna", sizeof(student5.name));
-  strncpy(student5.class_level, class_level_types[0],
-          sizeof(student5.class_level));
-  strncpy(student5.language, language_types[1], sizeof(student5.language));
-
-  Student student6;
-  student6.registration = generate_registration();
-  strncpy(student6.name, "Bob", sizeof(student6.name));
-  strncpy(student6.class_level, class_level_types[1],
-          sizeof(student6.class_level));
-  strncpy(student6.language, language_types[3], sizeof(student6.language));
-
-  Student student7;
-  student7.registration = generate_registration();
-  strncpy(student7.name, "Catherine", sizeof(student7.name));
-  strncpy(student7.class_level, class_level_types[2],
-          sizeof(student7.class_level));
-  strncpy(student7.language, language_types[0], sizeof(student7.language));
-
-  Student student8;
-  student8.registration = generate_registration();
-  strncpy(student8.name, "Henry", sizeof(student8.name));
-  strncpy(student8.class_level, class_level_types[2],
-          sizeof(student8.class_level));
-  strncpy(student8.language, language_types[1], sizeof(student8.language));
-
-  Student student9;
-  student9.registration = generate_registration();
-  strncpy(student9.name, "Xander", sizeof(student9.name));
-  strncpy(student9.class_level, class_level_types[1],
-          sizeof(student9.class_level));
-  strncpy(student9.language, language_types[1], sizeof(student9.language));
-
-  Student student10;
-  student10.registration = generate_registration();
-  strncpy(student10.name, "Zach", sizeof(student10.name));
-  strncpy(student10.class_level, class_level_types[2],
-          sizeof(student10.class_level));
-  strncpy(student10.language, language_types[3], sizeof(student10.language));
 
   root = include_student(root, student1);
   root = include_student(root, student2);
@@ -271,48 +193,34 @@ int test_include_student(void) {
   root = include_student(root, student10);
 
   printf("Names inserted in the tree:\n");
-  print_student_names(root);
+  list_all_students(root);
   printf("\n");
 
   if (is_tree_alphabetical(root)) {
     printf("Test include_student: Passed\n");
+    free_all_students_mocks();
     free_tree(root);
     return 0;
   } else {
     printf("Test include_student: Failed\n");
+    free_all_students_mocks();
+    free_tree(root);
     return 1;
   }
 }
 
 int test_list_all_students(void) {
-  Node* root = NULL;
-
-  Student student1;
-  student1.registration = generate_registration();
-  strncpy(student1.name, "Zoe", sizeof(student1.name));
-  strncpy(student1.class_level, class_level_types[0],
-          sizeof(student1.class_level));
-  strncpy(student1.language, language_types[2], sizeof(student1.language));
-
-  Student student2;
-  student2.registration = generate_registration();
-  strncpy(student2.name, "Alice", sizeof(student2.name));
-  strncpy(student2.class_level, class_level_types[2],
-          sizeof(student2.class_level));
-  strncpy(student2.language, language_types[0], sizeof(student2.language));
-
-  Student student3;
-  student3.registration = generate_registration();
-  strncpy(student3.name, "Charlie", sizeof(student3.name));
-  strncpy(student3.class_level, class_level_types[1],
-          sizeof(student3.class_level));
-  strncpy(student3.language, language_types[2], sizeof(student3.language));
+  initialize_students_mocks();
+  Node* root = initialize_tree();
 
   root = include_student(root, student1);
   root = include_student(root, student2);
   root = include_student(root, student3);
 
   list_all_students(root);
+
+  free_all_students_mocks();
+  free_tree(root);
 
   printf("Test list_all_students_simple: Passed\n");
   return 0;
