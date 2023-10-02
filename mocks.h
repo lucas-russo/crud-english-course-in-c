5111af6376cd8b86678d63f18716af8f30011aec
Hermano Castro <hermanocastro18@gmail.com>
2023-10-02 15:25:18 -0300

    create mock files with 10 students and function initialize_students()

---
+++ mocks.h
@@ -0,0 +1,19 @@
+#ifndef MOCKS_H
+#define MOCKS_H
+
+#include "tree.h"
+
+extern Student student1;
+extern Student student2;
+extern Student student3;
+extern Student student4;
+extern Student student5;
+extern Student student6;
+extern Student student7;
+extern Student student8;
+extern Student student9;
+extern Student student10;
+
+void initialize_students(void);
+
+#endif
