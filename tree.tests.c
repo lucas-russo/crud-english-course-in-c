66bba076dec3298c2f10d2bab23228907313356b
Hermano Castro <hermanocastro18@gmail.com>
2023-10-02 14:32:16 -0300

    fix test include students

--- tree.tests.c
+++ tree.tests.c
@@ -112,6 +112,7 @@ int test_free_node() {
   strcpy(root->student.name, "Charlie");
   strcpy(root->student.class_level, class_level_types[1]);
   strcpy(root->student.language, language_types[2]);
+  
   root->left = node1;
   root->right = node2;
 
@@ -139,8 +140,13 @@ int test_generate_registration() {
 }
 
 int test_create_node() {
-  Student student = {generate_registration(), "Alice", class_level_types[2],
-                     language_types[0]};
+  Student student;
+  student.registration = generate_registration();
+  strncpy(student.name, "Alice", sizeof(student.name));
+  strncpy(student.class_level, class_level_types[2],
+          sizeof(student.class_level));
+  strncpy(student.language, language_types[0], sizeof(student.language));
+
   Node* left = create_node(student, NULL, NULL);
   Node* right = create_node(student, NULL, NULL);
   Node* root = create_node(student, left, right);
@@ -150,8 +156,6 @@ int test_create_node() {
     return 1;
   }
 
-  free_node(left);
-  free_node(right);
   free_node(root);
 
   printf("Test create_node: Passed\n");
@@ -160,9 +164,10 @@ int test_create_node() {
 
 void print_student_names(Node* node) {
   if (node == NULL) return;
-
   print_student_names(node->left);
   printf("%s\n", node->student.name);
+  printf("%s\n", node->student.class_level);
+  printf("%s\n", node->student.language);
   print_student_names(node->right);
 }
 
@@ -186,26 +191,75 @@ int is_tree_alphabetical(Node* node) {
 int test_include_student(void) {
   Node* root = initialize_tree();
 
-  Student student1 = {generate_registration(), "Zoe", class_level_types[0],
-                      language_types[2]};
-  Student student2 = {generate_registration(), "Alice", class_level_types[2],
-                      language_types[0]};
-  Student student3 = {generate_registration(), "Charlie", class_level_types[1],
-                      language_types[2]};
-  Student student4 = {generate_registration(), "Yasmine", class_level_types[1],
-                      language_types[0]};
-  Student student5 = {generate_registration(), "Anna", class_level_types[0],
-                      language_types[1]};
-  Student student6 = {generate_registration(), "Bob", class_level_types[1],
-                      language_types[3]};
-  Student student7 = {generate_registration(), "Catherine",
-                      class_level_types[2], language_types[0]};
-  Student student8 = {generate_registration(), "Henry", class_level_types[2],
-                      language_types[1]};
-  Student student9 = {generate_registration(), "Xander", class_level_types[1],
-                      language_types[1]};
-  Student student10 = {generate_registration(), "Zach", class_level_types[2],
-                       language_types[3]};
+  Student student1;
+  student1.registration = generate_registration();
+  strncpy(student1.name, "Zoe", sizeof(student1.name));
+  strncpy(student1.class_level, class_level_types[0],
+          sizeof(student1.class_level));
+  strncpy(student1.language, language_types[2], sizeof(student1.language));
+
+  Student student2;
+  student2.registration = generate_registration();
+  strncpy(student2.name, "Alice", sizeof(student2.name));
+  strncpy(student2.class_level, class_level_types[2],
+          sizeof(student2.class_level));
+  strncpy(student2.language, language_types[0], sizeof(student2.language));
+
+  Student student3;
+  student3.registration = generate_registration();
+  strncpy(student3.name, "Charlie", sizeof(student3.name));
+  strncpy(student3.class_level, class_level_types[1],
+          sizeof(student3.class_level));
+  strncpy(student3.language, language_types[2], sizeof(student3.language));
+
+  Student student4;
+  student4.registration = generate_registration();
+  strncpy(student4.name, "Yasmine", sizeof(student4.name));
+  strncpy(student4.class_level, class_level_types[1],
+          sizeof(student4.class_level));
+  strncpy(student4.language, language_types[0], sizeof(student4.language));
+
+  Student student5;
+  student5.registration = generate_registration();
+  strncpy(student5.name, "Anna", sizeof(student5.name));
+  strncpy(student5.class_level, class_level_types[0],
+          sizeof(student5.class_level));
+  strncpy(student5.language, language_types[1], sizeof(student5.language));
+
+  Student student6;
+  student6.registration = generate_registration();
+  strncpy(student6.name, "Bob", sizeof(student6.name));
+  strncpy(student6.class_level, class_level_types[1],
+          sizeof(student6.class_level));
+  strncpy(student6.language, language_types[3], sizeof(student6.language));
+
+  Student student7;
+  student7.registration = generate_registration();
+  strncpy(student7.name, "Catherine", sizeof(student7.name));
+  strncpy(student7.class_level, class_level_types[2],
+          sizeof(student7.class_level));
+  strncpy(student7.language, language_types[0], sizeof(student7.language));
+
+  Student student8;
+  student8.registration = generate_registration();
+  strncpy(student8.name, "Henry", sizeof(student8.name));
+  strncpy(student8.class_level, class_level_types[2],
+          sizeof(student8.class_level));
+  strncpy(student8.language, language_types[1], sizeof(student8.language));
+
+  Student student9;
+  student9.registration = generate_registration();
+  strncpy(student9.name, "Xander", sizeof(student9.name));
+  strncpy(student9.class_level, class_level_types[1],
+          sizeof(student9.class_level));
+  strncpy(student9.language, language_types[1], sizeof(student9.language));
+
+  Student student10;
+  student10.registration = generate_registration();
+  strncpy(student10.name, "Zach", sizeof(student10.name));
+  strncpy(student10.class_level, class_level_types[2],
+          sizeof(student10.class_level));
+  strncpy(student10.language, language_types[3], sizeof(student10.language));
 
   root = include_student(root, student1);
   root = include_student(root, student2);
